abc8601df7a3521a2f3280879f7d8ef1d8b6e2e7
evanxuhe <evanxuhe@163.com>
2026-02-05 10:04:04 +0800

    增加wss打印

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,18 @@ export default defineConfig({
 			'/ws': {
 				target: 'ws://chat-test.mangotest.ai:8080',
 				changeOrigin: true,
-				ws: true
+				ws: true,
+				configure: (proxy) => {
+					proxy.on('error', (err) => {
+						console.log('proxy error', err);
+					});
+					proxy.on('proxyReq', (proxyReq, req) => {
+						console.log('Sending Request to the Target:', req.method, req.url);
+					});
+					proxy.on('proxyRes', (proxyRes, req) => {
+						console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+					});
+				}
 			}
 		}
 	},
